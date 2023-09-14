32e1b7f681b06c5d43f5e6d6d4db438de2891fbf
Saru <heo_dark@naver.com>
2023-09-14 15:09:16 +0900

    clean result

--- naver_flight_crawling.py
+++ naver_flight_crawling.py
@@ -8,7 +8,11 @@ from selenium.webdriver.support import expected_conditions as EC
 # 크롬 드라이버 자동 업데이트
 from webdriver_manager.chrome import ChromeDriverManager
 # 날짜 라이브러리
-from datetime import datetime
+from datetime import date, timedelta
+import mysql_auth
+import pymysql
+
+# 각종 설정
 
 # 브라우저 자동꺼짐 방지
 chrome_options = Options()
@@ -19,6 +23,8 @@ chrome_options.add_experimental_option("detach", True)
 chrome_options.add_experimental_option("excludeSwitches", ['enable-logging'])
 service = Service(executable_path='chromedriver.exe')
 driver = webdriver.Chrome(service=service, options=chrome_options)
+
+# 각종 만든 함수
 
 # 페이지 로딩 기다렸다가 클릭
 def like_text_click_wait(tag, elem):
@@ -43,12 +49,13 @@ driver.get(url)
 begin_date = driver.find_element(By.XPATH, '//button[text() = "가는 날"]')
 begin_date.click()
 # 출발 일정 으로 내일 날짜 클릭
-tomorrow = datetime.today().day + 1
+tomorrow = date.today().day + 1
 departDay = driver.find_elements(By.XPATH, '//b[text() = "{0}"]'.format(tomorrow))[0]
 departDay.click()
 # 도착 일정 으로 3박4일 날짜 클릭
 arriveDay = driver.find_elements(By.XPATH, '//b[text() = "{0}"]'.format(tomorrow + 4))[0]
 arriveDay.click()
+
 # 도착지 설정 클릭
 text_click_wait('b', '도착')
 # destination = driver.find_element(By.XPATH, '//b[text() = "도착"]')
@@ -57,6 +64,7 @@ text_click_wait('b', '도착')
 text_click_wait('button', '일본')
 # 도착 공항 간사이 공항 클릭
 text_click_wait('i', '간사이국제공항')
+
 # 해당 일정 으로 조회
 like_text_click_wait('span', '검색')
 
@@ -74,11 +82,49 @@ text_click_wait('span', '제외')
 ticket_info = WebDriverWait(driver, 30).until(EC.presence_of_all_elements_located((By.XPATH, '''//div[@class="concurrent_ConcurrentItemContainer__2lQVG result"]''')))
 
 # 티켓 리스트 생성
-ticket_list = [str(x+1)+" " + ticket_info[x].text.replace("\n", " ") for x in range(10)]
+ticket_list = [str(x+1) +" " + ticket_info[x].text.replace("\n", " ") for x in range(10)]
 
-# 티켓 리스트 출력
-print(*ticket_list, sep='\n')
-input("종료 하려면 아무키나 누르세요")
-driver.quit()
+# 공백 제거, 불필요한 데이터 제거
+replace_list = []
+for x in ticket_list:
+    replace_x = x.replace("성인/모든 결제수단", "").replace("이벤트혜택", "").replace("직항", "").replace("왕복", "").replace(",", "").replace("   ", " ").replace("  ", " ")
+    replace_list.append(replace_x)
 
-# 230906 TODO : 문장 분리해서 출발 (항공사, 시간, 출국공항, 소요시간) , 도착(항공사, 시간, 출국공항, 소요시간) , 가격 DB에 넣을 수 있게 데이터가공 필요
+login = mysql_auth.lnfo
+# MySQL 연결
+conn = pymysql.connect(host=login['host'], port=login['port'],
+                       user=login['user'], passwd=login['passwd'],
+                       db=login['db'], charset=login['charset'])
+# Cursor 생성
+cur = conn.cursor()
+
+sql = """
+        INSERT INTO BD_TICKET_CRAWLING
+        (TICKET_RANK, DEPT_DAY, DEPT_CITY, DEPT_AIRLINE, DEPT_TIME,
+         ARRV_DAY, ARRV_CITY, ARRV_AIRLINE, ARRV_TIME, PRICE)
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """
+
+for x in replace_list:
+    ticket = x.split(" ")
+    rank = ticket[0]
+    month = str(date.today().month)
+    today = date.today().day
+    depart_day = month + "월" + str(today + 1) + "일"
+    depart_airline = ticket[1]
+    depart_randing_time = ticket[2][:5]
+    depart_city = ticket[2][5:]
+    arrive_day = month + "월" + str(today + 5) + "일"
+    if ticket[6][0].isdigit():
+        ticket.insert(6, depart_airline)
+    arrive_airline = ticket[6]
+    arrive_randing_time = ticket[7][:5]
+    arrive_city = ticket[7][5:]
+    price = ticket[-1]
+
+    # SQL 실행
+    cur.execute(sql, (rank, depart_day, depart_city, depart_airline, depart_randing_time, arrive_day, arrive_city, arrive_airline, arrive_randing_time, price))
+# 데이터 커밋
+conn.commit()
+# 연결 종료
+conn.close()
